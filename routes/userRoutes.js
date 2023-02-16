3a38423cbea683335e572f3e16e6edc5f40af148
adityachandra1 <adityachandra9898@gmail.com>
2023-02-16 13:55:29 +0530

    boilerplate-added

---
+++ routes/userRoutes.js
@@ -0,0 +1,32 @@
+const express = require("express");
+
+// Importing the controller functions.
+const {
+    getAllUsers,
+    getUserByID,
+    createUser,
+    updateUser,
+    deleteUser
+} = require("../controllers/userControllers");
+
+// Import the required middlware here.
+const { /*...*/ } = require(/*...*/);
+
+const router = express.Router();
+
+/*
+Add access control middleware to the existing GET and POST routes, as follows: 
+- The getAllUsers route should be accessible to both users and admins.
+- The createUser route should only be accessible to admins.
+- The getUserByID route should be accessible to both users and admins.
+*/
+
+// Add the middleware here
+router.get("/", /*...*/ getAllUsers);
+router.post("/", /*...*/ createUser);
+router.get("/:id", /*...*/ getUserByID);
+
+router.patch("/:id", updateUser);
+router.delete("/:id", deleteUser);
+
+module.exports = router;
